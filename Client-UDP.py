eaea896c1687924c38c054e5d05eb17c6b91bd38
gharper19 <gharper1996@gmail.com>
2019-04-10 19:22:41 -0500

    Cleaned up code and added better comments

--- Client-UDP.py
+++ Client-UDP.py
@@ -12,9 +12,8 @@ class ClientThread (threading.Thread):
         self.threadID = threadID
         self.name = name
         self.counter = counter
+    
     def run(self):
-        # print("Starting %s" % self.name)
-        
         global stayOnServer
         global inbox
         global ctrl_inbox
@@ -29,24 +28,28 @@ class ClientThread (threading.Thread):
                 # Acquire thread lock to assure this runs with main thread
                 threadLock.acquire()
 
-                # Check for message break
+                # Check for message break and seperate tokens
                 if msg_break in clientData:
                     prefix, data = clientData.split(msg_break)
                 else:
                     data = clientData
                 
+                # Check message for control responses
                 if "#./USER" in prefix:
                     ctrl_inbox += [clientData]
                     print("[INFO] CTRL Inbox: " + str(ctrl_inbox))
                 elif "#./EXIT" in prefix:
                     stayOnServer = False
+                elif "#./ERROR_INVALID_USER" in prefix:
+                    ctrl_inbox += [clientData]
+                    print("Recent message '%s' failed to send. Destination user was not found in active userlist."
+                        % data)
                 else: 
                     inbox += [data]
-                    print("[INFO] MSG Inbox: " + str(inbox))        # Issue printing while taking input?
+                    time.sleep(2)
+                    print("[INFO] MSG Inbox: " + str(inbox))        
                 
-                # Acquire thread lock to assure this runs with main thread
                 threadLock.release()
-            
             except Exception as e:
                 print("Error receiving messages from server %s: %s" % (str(server) , e))
 
@@ -61,8 +64,9 @@ name = ''
 inbox=[]
 ctrl_inbox = []
 
-## Get user input
+
 def init_client():
+# Get user data for establishing connection and request username and userlist from server 
     # Init and bind client socket
     clientHost = '127.0.0.1'
     clientPort = 9998
@@ -71,7 +75,7 @@ def init_client():
     
     # Get Server IP and port
     host = '127.0.0.1'   #'192.168.56.1'#input(str("Please enter the server address : "))#
-    port = 9997 # Or should it be 1024 (server rcv-from port?) 
+    port = 9997
     server = (host,port)
 
     # Get username
@@ -81,111 +85,102 @@ def init_client():
     print("Connecting to UDP Chat Server at %s:%d ..." % ( host, port) )
     time.sleep(1)
     print("Requesting username %s ... \n" % name)
+    time.sleep(1)
     pkt = "#./INIT" + msg_break + name
     clientSock.sendto(pkt.encode("ascii"), server)
 
     # Wait for userId and user list, then parse
-    msg = clientSock.recvfrom(1026)[0]
-    msg = msg.decode('utf-8')
-   
     try:   
+        msg = clientSock.recvfrom(1026)[0]
+        msg = msg.decode('utf-8')
         name, userlist = msg.split(msg_break)
         print("Connected to Chat Server. Your screen name is %s" % name)
         print(userlist)
     except Exception as e: 
-        print("Error Initiating with server." + str(e))
+        print("Error initiating chat client with server." + str(e))
         raise e
     return clientSock, server
 
 def reqUserList():
+# Send request to server for most recent active userlist update
     global inbox
-    
-    # Request User List with appended random number to determine most recent
     userlist=''
     check, check_inbox_limit= 0, 7
     
+    # Generate random control key to append to message to verify update is new  
     update_key = random.randint(1000, 9999)
     req = "#./USER" + msg_break + str(update_key)
     prefix ="#./USER" + str(update_key)
+    
+    # send request
     try:
         clientSock.sendto(req.encode("ascii"), server)
         print("Requesting updated user list ...\n")
     except Exception as e:
         print("Error requesting user list: %s" % e)
 
-    # Give receiver thread time to add to control inbox and check for response
+    # Give client listener thread time to add response to ctrl_inbox, then check for response
     while check <= check_inbox_limit:
         time.sleep(resp_wait)
         for msg in ctrl_inbox:
             msg_prefix, userdata = msg.split(msg_break)
             if msg_prefix == prefix:
-                return userdata
+                userlist = userdata
+                break
         if check == check_inbox_limit and userlist == '':
-            return "Updated Userlist not found in inbox after waiting %d seconds for %d iterations" % (resp_wait, check_inbox_limit)
-        # if check < 2:
-        #     input("Continue? Press Enter")
+           print("Updated Userlist not found in inbox after waiting %d seconds for %d iterations : %s" % (resp_wait, check_inbox_limit, userlist))
         check +=1  
-        print("Inbox check %d: %s" % (check, ctrl_inbox))
-
+    
 def main_loop():
 # Start thread to listen for server messages while also waiting for user input
-    listening_thread = ClientThread(threadID=1, name="Listening Thread", counter=1)
-    listening_thread.start()
-
     global stayOnServer
     global inbox
+    initial_setup = True
 
-    setDest = True
-    debug_flag=1
-    while stayOnServer: 
-        # Check if this is first loop iteration
-        if setDest:
-        ## Include list of delimited clients as raw_input option for group messages 
-            
+    # Start thread to listen for server responses
+    listening_thread = ClientThread(threadID=1, name="Listening Thread", counter=1)
+    listening_thread.start()
+    
+    # Start user interface loop
+    while stayOnServer:    
+        if initial_setup:
+        # Check if this is first loop iteration, if so get initial destination to initialize destination user before continuing with main loop
             notValid = True
-            # print("# %s #\n"%userlist)
             while notValid: 
                 dest = ''
-                print("Enter the name of the user you want to message. Enter none to pick later.")
+                print("Enter the name of the user you want to message. Enter 'none' or '0' to pick later.")
                 try:    
                     dest = raw_input().strip()
                 except Exception as e:
                     dest = input()
                 notValid = False
-                if dest == './user' or dest == '':
+                if dest == './user':
                     print('Invalid username, please select a user from the active users list.')
                     notValid = True
-                elif dest =='none':
+                elif dest =='none' or dest =='no one' or dest =='0' or dest == '' or dest == './exit':
                     print("No user selected. Enter ./user to select a user to message.")
                     dest = "no one"
             print("Currently messaging %s" % dest)
-            setDest= False
-
-        if not setDest:
+            initial_setup= False
+        elif not initial_setup:
+        # if this is not the first loop iteration, dest is already initiated, so continue interface loop
             print("\nEnter your message for " + dest 
-                + ". Enter ./user to change user and ./exit to leave chat server. Enter ./inbox to view your message inbox.")
+                + ". Enter './user' to change user and './inbox' to view your message inbox. \nEnter ./exit to leave chat server .")
+            
+            # Included try/catch in all user inputs for conflicting python versions
             try:    
                 msg = raw_input().strip()
             except Exception as e:
                 msg = input()
-        
-        # if debug_flag == 1:
-        #     msg = './user'
-        #     debug_flag += 1
-        # elif debug_flag == 2:
-        #     time.sleep(1)
-        #     msg = './inbox'
-        # elif debug_flag > 5: 
-        #     # End early
-        #     return
 
-        # Check for control commands
         if msg == './user':
-        # If user is requesting user list then send request and get destination input 
+        # If user is requesting user list then send request and get new user destination input 
+            
+            # Request userlist from server and print it
             userlist = reqUserList()
             print("# %s #\n"%userlist)
 
-            ## Include list of delimited clients as raw_input option for group messages 
+            # Get user destination input and set dest
             notValid = True
             while notValid: 
                 dest = ''
@@ -195,6 +190,7 @@ def main_loop():
                 except Exception as e:
                     dest = input()
                 notValid = False
+                
                 if dest == './user' or dest == '':
                     print('Invalid username, please select a user from the active users list.')
                     notValid = True
@@ -204,10 +200,15 @@ def main_loop():
             print("Currently messaging %s" % dest)
         
         elif msg == './inbox': 
-            print("Chat Message Inbox:\n")
+        # display all user messages received during this session 
+            print("Chat Message Inbox:")
             if len(inbox) == 0: print("No new messages received.")
+            else:
+                for m in inbox:
+                    print("  %s -> \n")
             
         elif msg == './exit': 
+        # Send exit message to server and end main loop
             stayOnServer = False
             pkt = "#./EXIT" + msg_break + " #./CONFIRM" 
             print("\nDisconnecting from server ...")
@@ -216,7 +217,7 @@ def main_loop():
         else: 
         # If no control messages, then send message to preset destination user
             if dest == name:
-                print("Error, you entered your name as the recieving user")
+                print("Error, you entered your name as the receiving user")
                 pass
             elif dest == 'no one':
                 print("Error - You have yet to select a user to message. Enter ./user to select a receiving user from the user list.")
@@ -236,13 +237,17 @@ def main_loop():
                         clientSock.sendto(pkt.encode('ascii'), server)
                     except Exception as e: 
                         print("Error sending client msg '%s' to server: %s" % (msg, e) )
-                    print("Message sent!")
+                    print("Message sent.")
 
+    
     print("GoodBye!\nPress Enter to Close.")
     try:    
         raw_input().strip()
     except Exception as e:
         input()
+
+interface_timing_wait = 1
+
 # Create thread lock to sync threads
 threadLock = threading.Lock()
 
--- Server-UDP.py
+++ Server-UDP.py
@@ -22,17 +22,6 @@ class ServerThread (threading.Thread):
         else: 
             handle_user(self.clientAddr, self.clientData, self.name)
 
-'''
-# Create new threads
-thread1 = myThread(1, "Thread-1", 1)
-thread2 = myThread(2, "Thread-2", 2)
-
-# Start new Threads
-thread1.start()
-thread2.start()
-
-'''
-
 
 ################## Server Code ####################
 
@@ -44,6 +33,7 @@ thread_limit = 99
 users = {}
 IPs = {}
 msg_break =' /$MESSAGE_BREAK: '
+remoteKillServerCmd = "#./KILLSERVER"
 
 # Initialize Server Socket as UDP 
 serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -53,6 +43,7 @@ serverHost = '127.0.0.1' # socket.gethostbyname(socket.gethostname()) #
 serverPort = 9997
 serverSock.bind((serverHost, serverPort))
 bufferSize = 1024
+
         
 def init_user(clientAddr, clientData, threadName):
 # Add client to lists and send back username and IP address
@@ -75,23 +66,37 @@ def init_user(clientAddr, clientData, threadName):
     print("[INFO] [INIT_USER] %s: New client %s:%d assigned username: %s" % (threadName, clientAddr[0], clientAddr[1], username) ) 
 
 def handle_user(clientAddr, clientData, threadName):
+# Check that user destination is valid, then send message to destination 
     print("[INFO] [SND_MSG] %s: Message Received from %s:%d - %s" % (threadName, clientAddr[0], clientAddr[1], clientData.decode('utf-8') ))
     
     # Separate Client UDP Data into rcv user and actual message data 
-    rcv_user, user_msg = clientData.decode('utf-8').split(msg_break)[0], clientData.decode('utf-8').split(msg_break)[1]
+    rcv_user, user_msg = clientData.decode('utf-8').split(msg_break)[0],clientData.decode('utf-8').split(msg_break)[1]
+    
+    # check if rcv_user is valid, if not respond with error code and exit function
+    if rcv_user == IPs[clientAddr] or rcv_user not in users.keys():
+        resp = "#./ERROR_INVALID_USER" + msg_break + clientData
+        serverSock.sendto(resp.encode('ascii'), clientAddr)
+        print("[ERROR] [SND_MSG] Destination user is invalid. Cannot send message '%s':%s" 
+            % (clientData.decode('utf-8'), str(e) ) )
+        return
+   
+    # Send users message to intended user
     try: 
-        # ! Add error handling for user not found and user is self
         message = "From " + IPs[clientAddr] + ': ' + user_msg      
         rcv_addr, rcv_port =  users[rcv_user]         
-        print("[INFO] [SND_MSG] User %s messaging %s at %s:%d .." % (IPs[clientAddr], rcv_user , rcv_addr, rcv_port))
         serverSock.sendto(message.encode('ascii'), (rcv_addr, rcv_port))
+        print("[INFO] [SND_MSG] User %s has sent message '%s' to  %s at %s:%d .." 
+            % (IPs[clientAddr], user_msg, rcv_user , rcv_addr, rcv_port)) 
     except Exception as e: 
-        print("Error Handling User Messaging: " + str(e))
+        print("[ERROR] [SND_MSG] Error sending user message '%s':%s" 
+            % (clientData.decode('utf-8'), str(e) ) )
 
-    # Send message to receiving IP address and port
-    print("Message Sent!"); 
-
-def getUserList(clientName = ''):
+def sendUserList(clientAddr, clientData):
+# Compile all usernames into a string, mark user with '(you)', and send to requesting user 
+    clientData = clientData.decode("utf-8")
+    clientName = IPs[clientAddr]
+    
+    # Compile User list
     userlist = 'Active Users: '
     usernames = list(users.keys())
     if len(usernames) == 0:
@@ -109,26 +114,22 @@ def getUserList(clientName = ''):
                         userlist += name + "(you)" + ' - '
                     else:
                         userlist += name + ' - '
-                # print("[INFO] userlist successfully retrieved in getUserList")
             except Exception as e: 
-                print("[ERROR] -- Userlist Empty -- KeyError with list concatentation in getUserList")
+                print("[ERROR] -- Userlist Empty -- KeyError with list concatentation in getUserList: " + e)
                 userlist = '[Empty]'
-    return userlist
 
-def sendUserList(clientAddr, clientData):
-    clientData = clientData.decode("utf-8")
-    userlist = getUserList(IPs[clientAddr])
+    # Compile Ctrl prefix from Ctrl msg and update code
     msg_prefix = clientData.split(msg_break)[0] + clientData.split(msg_break)[1]
     
-    # return userlist with user control commad
-    resp = msg_prefix + msg_break + userlist
+    # return userlist with user control id confirmation
     try:
+        resp = msg_prefix + msg_break + userlist
         serverSock.sendto(resp.encode('ascii'), clientAddr)            
     except Exception as e:
         print("[ERROR] [SEND_USERLIST] Error Sending user list: %s" % e)
 
-# ! strip() strings on user input
 def main_loop():
+# Begin serving clients
     print("Server Online at %s:%s... Waiting for connections: \n" % (serverHost, serverPort) )
     threadCount = 1
     
@@ -140,9 +141,11 @@ def main_loop():
         except Exception as e:
             print("[ERROR MAIN] Error receiving client datagram with buffer size %d: %s" % (bufferSize, str(e)))
         
+
         # Debugging message
         print("[INFO MAIN] Recieved datagram: %s - From: %s:%d" % (clientData, clientAddr[0], clientAddr[1]))
-        
+
+
         if msg_break in clientData.decode('utf-8'):
             msg_prefix, clientMsg = clientData.decode('utf-8').split(msg_break)
         else:
@@ -150,22 +153,30 @@ def main_loop():
             msg_prefix = ''
             clientMsg = ''
 
-        # Check for empty messages
+        # Check for empty messages or control messages
         if clientMsg == "":
-            break  # For debugging
-        # Check for control messages 
+            pass
+        
+        elif clientMsg == remoteKillServerCmd:
+        # Admin control command to terminate server 
+            break
+        
         elif msg_prefix == "#./INIT": 
-        # If new user, start thread recieve username and add to user listen else send user's message
+        # If new user, start new thread to receive username, add client to user list
             s = ServerThread(threadCount, "Thread-%d" % threadCount, threadCount, 
                 clientAddr, clientMsg, newUser=True)
             threadCount+=1
             s.start()
+        
         elif msg_prefix == '#./USER':
+        # Send user current userlist with requesting user marked with '(you)'
             s = ServerThread(threadCount, "Thread-%d" % threadCount, threadCount, 
                 clientAddr, clientData, newUser=False, sendUsers=True)
             threadCount+=1
             s.start()
+        
         elif msg_prefix == '#./EXIT':
+        # Remove user from userlist and respond with confirmation of disconnection
             try:
                 print("[INFO] User %s at %s:%d is disconnecting" % (IPs[clientAddr], clientAddr[0], clientAddr[1]))
                 del users[IPs[clientAddr]]
@@ -178,27 +189,16 @@ def main_loop():
                 print("[INFO] Client %s:%d successfully disconnected." % (clientAddr[0], clientAddr[1]))
             except Exception as e:
                 print("Error: Exiting user not found in userlist: %s" % e)
+        
         elif clientAddr in users.values():
-        # If host IP is in userlist then start thread to send user's message 
-
+        # if user is in active user list start thread to send user's message to destination
             # handle user not found and multiple users
             s = ServerThread(threadCount, "Thread-%d" % threadCount, threadCount, 
                 clientAddr, clientData, newUser=False)
             threadCount+=1
             s.start()
-        
 
-
-# socket.setdefaulttimeout(timeout) 
-
-# def if __name__ == "__main__":
-#     pass
 try:
     main_loop()
 except Exception as e: 
     input("Server FAILURE: %s\nPress Enter to Exit" % e)
-
-# TODO: 
-
-# BUGS:
-# - Check on getUserList() dictionary key error  
